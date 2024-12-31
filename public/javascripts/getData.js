f8e8cd6fde04ef2fd9d2800684c127ee2a26616f
mrlyov <vpoghosyan>
2024-12-31 21:14:45 +0400

    Изменена логика добавления данных в getData.js

--- public/javascripts/getData.js
+++ public/javascripts/getData.js
@@ -1,17 +1,22 @@
 function getData(){
     const userInfo = JSON.parse(localStorage.getItem('userInfo') || '{}');
-    if (!userInfo || !userInfo.id || !userInfo.name || !userInfo.image){
-        fetch('/getData', {
-            method: 'post',
-            headers: {
-                'Content-type': 'application/json'
-            },
+    fetch('/getData', {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json'
+        },
+    })
+        .then(response => response.json())
+        .then(data => {
+            let {user, image} = data;
+            if (!userInfo.id || userInfo.id !== user.id || userInfo.name !== user.name || userInfo.profileImage !== `data:image/png;base64,${image}`){
+                localStorage.setItem('userInfo', JSON.stringify({
+                    'id': user.id,
+                    'name': user.name,
+                    'profileImage': 'data:image/png;base64,' + image
+                }));
+                window.location.reload();
+            }
         })
-            .then(response => response.json())
-            .then(data => {
-                let {user, image} = data;
-                localStorage.setItem('userInfo', JSON.stringify({ 'id': user.id, 'name': user.name, 'profileImage': 'data:image/png;base64,' + image }));
-            })
-    }
 }
 getData();
